e6fa7bdd3b670b39f2be9ab1ecba2faeb4682acb
Technologia2014 <eunicemaabenayayra@gmail.com>
2025-08-26 08:10:16 +0000

    changes made to hero-page

--- index.js
+++ index.js
@@ -4,6 +4,35 @@ import {
   scroll,
 } from "https://cdn.jsdelivr.net/npm/motion@latest/+esm";
 
+//hero-page typewriter
+const dynamicText = document.querySelector(".hero-heading span");
+const words =["make bugs cry","code","design","have fun doing it!🤖"];
+
+let wordIndex = 0;
+let charIndex = 1;
+let isDeleting = false;
+
+const typeEffect =() => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChar;
+    dynamicText.classList.add("stop-blinking")
+
+    if(!isDeleting && charIndex < currentWord.length){
+        //If condition is true, type the next character
+        charIndex++;
+        setTimeout(typeEffect, 200);
+    }else if(isDeleting && charIndex > 0){
+         //If condition is true, remove the previous character
+        charIndex--;
+        setTimeout(typeEffect, 100);
+    }else{
+        isDeleting = !isDeleting;
+        wordIndex = !isDeleting? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200)
+    }
+}
+typeEffect();
 //Circle mouse movement
 // Select the circle element
 const circleElement = document.querySelector('.circle');
